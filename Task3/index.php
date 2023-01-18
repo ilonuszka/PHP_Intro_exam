fdf97e6639eccfc2343ddd9a3888e9e6eda50680
4porthub <109093472+ilonuszka@users.noreply.github.com>
2023-01-18 19:32:24 +0200

    Task3 complete

--- Task3/index.php
+++ Task3/index.php
@@ -12,3 +12,58 @@
 */
 
 declare(strict_types=1);
+
+$holidays = [
+    [
+        'title' => 'Romantic Paris',
+        'destination' => 'Paris',
+        'price' => 1500,
+        'tourists' => 55,
+    ],
+    [
+        'title' => 'Amazing New York',
+        'destination' => 'New York',
+        'price' => 2699,
+        'tourists' => 21,
+    ],
+    [
+        'title' => 'Spectacular Sydey',
+        'destination' => 'Sydey',
+        'price' => 4130,
+        'tourists' => 9,
+    ],
+    [
+        'title' => 'Hidden Paris',
+        'destination' => 'Paris',
+        'price' => 1700,
+        'tourists' => 10,
+    ],
+    [
+        'title' => 'Emperors of the past',
+        'destination' => 'Beijing',
+        'price' => null,
+        'tourists' => 10,
+    ],
+];
+
+function printTripSummary (array $holidays): void {
+
+    $destinations = [];
+
+    foreach ($holidays as $item) {
+        if (!is_null($item['price'])) {
+            $destinations[$item['destination']]['title'][] = '"'.$item['title'].'"';
+            $destinations[$item['destination']]['total'][] = $item['price']*$item['tourists'];
+        }
+    }
+
+    foreach ($destinations as $key => $item) {
+        echo 'Destination "'.$key.'".'.PHP_EOL;
+        echo 'Titles: '.implode(', ',$item['title']).PHP_EOL;
+        echo 'Total: '.array_sum($item['total']).PHP_EOL;
+        echo '************'.PHP_EOL;
+    }
+
+}
+
+printTripSummary($holidays);
