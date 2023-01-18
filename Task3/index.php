7a4f8500687b0ce75d06ce753fce738c821444d0
4porthub <109093472+ilonuszka@users.noreply.github.com>
2023-01-18 18:59:27 +0200

    Start working on task3

---
+++ Task3/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+3.  Masyve $holidays turime kelionių agentūros siūlomas keliones su kaina ir dalyvių skaičiumi.
+    Terminale išspausdinkite santrauką, kurioje matytusi miesto pavadinimas, kelionių pavadinimai ir dalyvių sumokėta suma
+    Dėmesio! Santraukoje nerodykite tų kelionių, kurių kaina yra null. (3 balai)
+
+    Destination "Paris".
+    Titles: "Romantic Paris", "Hidden Paris"
+    Total: 99500
+    ************
+    Destination "New York"
+*/
+
+declare(strict_types=1);
