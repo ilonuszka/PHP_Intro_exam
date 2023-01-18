9193a0dbd36739e4ddab9becff7124fc3af4004c
4porthub <109093472+ilonuszka@users.noreply.github.com>
2023-01-18 18:54:56 +0200

    Task2 completed

---
+++ task1/index.php
@@ -0,0 +1,27 @@
+<?php
+// 1.  Grąžinkite visų lyginių skaičių, esančių $numbers masyve, sumą (1 balas)
+
+declare(strict_types=1);
+
+$numbers = [
+       15,
+       55,
+       66,
+       91,
+       100,
+       995,
+       17,
+       550,
+   ];
+
+function getEvenSum (array $numbers): int {
+
+    $result = 0;
+
+    foreach ($numbers as $number) 
+        $result += ($number%2)?0:$number;
+
+    return $result;
+}
+
+echo getEvenSum ($numbers);
